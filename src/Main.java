308559d5124a2d7b820f9874683c33bfbe916a6b
ShEO86 <130483030+ShEO86@users.noreply.github.com>
2023-04-12 22:35:06 +0500

    Домашнее задание день 8

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+    int deliveryDistance = 95;
+    int days = deleveryDays(deliveryDistance);
+        if (days != 0) {
+            System.out.printf("Потребуется дней: %d", days);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
+
+
+
+    }
+
+    public static int deleveryDays(int dist) {
+        int day = 1;
+        if (dist > 80) {
+            day = 0;
+            return day;
+        } else {
+            for (; dist >= 20; dist -= 20, day++ ) {
+            }
+        }
+
+    return day;
+    }
+}
